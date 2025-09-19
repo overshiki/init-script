03b4152339d90f467f17bfd27520f6846c6eab62
overshiki <le.niu@hotmail.com>
2025-09-20 00:28:49 +0800

    add racket

---
+++ racket/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -c https://download.racket-lang.org/installers/8.18/racket-8.18-x86_64-linux-buster-cs.shracket-8.18-x86_64-linux-buster-cs.sh
+bash racket-8.18-x86_64-linux-buster-cs.sh
