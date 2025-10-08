f4f16236afb4ca9d51672e396f5947096b17cf66
overshiki <le.niu@hotmail.com>
2025-10-08 18:05:51 +0800

    add tmux

---
+++ tmux/deps.rkt
@@ -0,0 +1,5 @@
+#lang racket
+(system "mkdir -p ~/.config/tmux/plugins/catppuccin")
+(system "mkdir -p ~/.config/tmux/plugins/catppuccin")
+(system "~/.config/tmux/plugins/catppuccin/tmux/catppuccin.tmux")
+(system "tmux source ~/.tmux.conf")
